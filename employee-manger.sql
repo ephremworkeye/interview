9e6323265135755a6d6d39c94d236d9787185c4b
Ephrem Wube <ephremworkeye@gmail.com>
2022-07-08 19:16:50 -0700

    emp-manager updated

--- employee-manger.sql
+++ employee-manger.sql
@@ -1,3 +1,33 @@
+-- creating and populating table
+--Create table Employees
+--(
+-- EmployeeID int primary key identity,
+-- EmployeeName nvarchar(50),
+-- ManagerID int foreign key references Employees(EmployeeID)
+--)
+--GO
+
+--Insert into Employees values ('John', NULL)
+--Insert into Employees values ('Mark', NULL)
+--Insert into Employees values ('Steve', NULL)
+--Insert into Employees values ('Tom', NULL)
+--Insert into Employees values ('Lara', NULL)
+--Insert into Employees values ('Simon', NULL)
+--Insert into Employees values ('David', NULL)
+--Insert into Employees values ('Ben', NULL)
+--Insert into Employees values ('Stacy', NULL)
+--Insert into Employees values ('Sam', NULL)
+--GO
+
+--Update Employees Set ManagerID = 8 Where EmployeeName IN ('Mark', 'Steve', 'Lara')
+--Update Employees Set ManagerID = 2 Where EmployeeName IN ('Stacy', 'Simon')
+--Update Employees Set ManagerID = 3 Where EmployeeName IN ('Tom')
+--Update Employees Set ManagerID = 5 Where EmployeeName IN ('John', 'Sam')
+--Update Employees Set ManagerID = 4 Where EmployeeName IN ('David')
+--GO
+
+
+
 declare @id int
 set @id = 4;
 
